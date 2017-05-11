87455060d9aefabd0af180c24508eff241e5a32c
Albert1994 <alfaalbert1994@gmail.com>
2017-05-11 14:21:10 +0300

    fsdfsd

--- src/Person.kt
+++ src/Person.kt
@@ -3,7 +3,13 @@
  */
 
 
-class Person(val name: String){
+class Person(val name: String): Comparable<Person> {
+    override fun compareTo(other: Person): Int {
+        if(this.age > other.age) return 1
+        else if (this.age < other.age) return  -1
+        return 0
+    }
+
     var age:Int = 0
     var isAdult = 0
 
@@ -24,16 +30,24 @@ class Person(val name: String){
 
         return str
     }
+    fun isAdult(): Boolean {
+        return this.age in 50..100
+    }
+    fun isAdult1(): Boolean {
+        return this.age > 18
+    }
+
 
 }
 
 fun main(args: Array<String>){
     var lord = Person("Lord")
-    var lord2 = Person("Lord2", 2)
+    var lord2 = Person("Lord2", 50)
 
 
     println(lord2.print_Person())
     println(lord.print_Person())
-
+    println(lord2.isAdult())
+    println(lord2.isAdult1())
 
 }
